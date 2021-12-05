6484c0483d086d4f95f32a33c7211605b04fe7f1
Haley Hamill <hamillhr@gmail.com>
2021-12-05 17:07:26 -0500

    added descent img to solutions

--- cfgFinalProject/scripts.js
+++ cfgFinalProject/scripts.js
@@ -4,3 +4,15 @@ img.src = "worldMap.svg";
 var div = document.getElementById("worldMap");
 div.appendChild(img);
 //block.setAttribute("style", "text-align:center");
+
+window.onscroll = function () {
+  myFunction();
+};
+
+function myFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("myP").className = "test";
+  } else {
+    document.getElementById("myP").className = "";
+  }
+}
