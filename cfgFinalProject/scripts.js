18c1d88704230f8d6764f577ab3091bd585d04ae
Haley Hamill <hamillhr@gmail.com>
2021-12-05 09:09:39 -0500

    code for inputting svg

--- cfgFinalProject/scripts.js
+++ cfgFinalProject/scripts.js
@@ -1,11 +1,6 @@
-function onClickImage(imageNumber) {
-    console.log("IMAGE CLICKED " + imageNumber);
-  
-    if (imageNumber == 3) {
-      console.log("IMAGE 3 CLICKED " + imageNumber);
-      window.location.href = "https://www.bbc.co.uk/sport";
-    } else if (imageNumber == 4) {
-      console.log("IMAGE 4 CLICKED " + imageNumber);
-      window.location.href = "https://www.theguardian.com/uk";
-    }
-  }
+var img = document.createElement("img");
+img.src = "worldMap.svg";
+
+var div = document.getElementById("worldMap");
+div.appendChild(img);
+//block.setAttribute("style", "text-align:center");
