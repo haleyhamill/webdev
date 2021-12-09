6de87325851fedff1d3ea5f287546cc31f9d0f52
Haley Hamill <hamillhr@gmail.com>
2021-12-09 15:39:45 -0500

    sample usages of Button component

---
+++ supermarketAppReact/supermarket/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import React from "react";
+import Button from "./Button.js";
+
+function App() {
+  function handleButtonClick() {
+    console.log("Button clicked");
+  }
+
+  return(<div style={{display: "flex", alignItems: "center", gap: "20px", flexWrap: "wrap"}}>
+    <Button>Normal</Button>
+    <Button outline>Outline</Button>
+    <Button className="extra-class" onClick={handleButtonClick}>Customizable</Button>
+  </div>);
+}
+
+export default App;
