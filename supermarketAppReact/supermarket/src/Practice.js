abc1ac556087a62be0206b39ee18e4d642eafcc3
Haley Hamill <hamillhr@gmail.com>
2021-12-11 21:23:07 -0500

    added nested JSX

--- supermarketAppReact/supermarket/src/Practice.js
+++ supermarketAppReact/supermarket/src/Practice.js
@@ -1,9 +1,14 @@
 import React from "react";
 
-function getPaymentButton(user) {
-    let name = "disabled";
-    if (user.is_loggedin) {
-        name = "clickable";
-    }
-    return <button className={name}>Pay</button>;
+function getPaymentMethods() {
+    return (
+        <select className="payment-methods">
+            <option className="payment-method">Card</option>
+            <option className="payment-method">Paypal</option>
+            <option className="payment-method">Cash on delivery</option>
+        </select>
+    );
 }
+
+// sample usage (do not modify)
+console.log(getPaymentMethods());
