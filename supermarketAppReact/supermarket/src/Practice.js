3dd47eacb8c33eef24e9fd530a842e4e22bb017a
Haley Hamill <hamillhr@gmail.com>
2021-12-11 21:53:18 -0500

    added react fragments

--- supermarketAppReact/supermarket/src/Practice.js
+++ supermarketAppReact/supermarket/src/Practice.js
@@ -1,14 +1,18 @@
 import React from "react";
 
-function getPaymentMethods() {
-    return (
-        <select className="payment-methods">
-            <option className="payment-method">Card</option>
-            <option className="payment-method">Paypal</option>
-            <option className="payment-method">Cash on delivery</option>
-        </select>
-    );
+function getFooter() {
+    return(
+    <>
+        <h3>Your online supermarket</h3>
+        <ul>
+            <li>Contact</li>
+            <li>Press</li>
+            <li>About</li>
+        </ul>
+        <p>All rights reserved &copy;</p>
+    </>
+    )
 }
 
 // sample usage (do not modify)
-console.log(getPaymentMethods());
+console.log(getFooter());
