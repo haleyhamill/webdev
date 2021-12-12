e9bd5048bb282e8b7bc200d1e944382bd5d0b81f
Haley Hamill <hamillhr@gmail.com>
2021-12-12 14:14:25 -0500

    added props to GreetUser

--- supermarketAppReact/supermarket/src/Practice.js
+++ supermarketAppReact/supermarket/src/Practice.js
@@ -1,21 +1,5 @@
 import React from "react";
-import {render} from "react-dom";
 
-function Hero() {
-    return(
-        <>
-            <h1>Welcome to our supermarket</h1>
-            <p>Start shopping</p>
-        </>
-    )
-};
-
-const root = document.querySelector("#react-root");
-
-render(<Hero/>, root);
-
-
-
-
-// do not modify
-render(<Hero></Hero>, document.querySelector("#react-root"));
+export default function GreetUser(props) {
+    return <div>Welcome {props.user}</div>
+}
