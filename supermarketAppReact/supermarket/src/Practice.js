2701305bf71ef3ec98a8491590f5fd3fbede54df
Haley Hamill <hamillhr@gmail.com>
2021-12-12 21:58:02 -0500

    changed props immutably

--- supermarketAppReact/supermarket/src/Practice.js
+++ supermarketAppReact/supermarket/src/Practice.js
@@ -1,7 +1,13 @@
 import React from "react";
 
-function WelcomeUser(props) {
-    const {username, notifications} = props;
-    
-    return <div>Welcome {username}! You've got {notifications} unread notifications.</div>;
+function Notifications(props) {
+    const value = props.data.count - 1;
+    return <h3>You have {value} unread notifications.</h3>;
 }
+
+const notifications = {
+    count: 3
+};
+
+const element = <Notifications data={notifications} />;
+console.log(notifications);
