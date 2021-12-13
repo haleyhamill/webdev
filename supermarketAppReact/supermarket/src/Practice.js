7dc038b51a7420a163430f7b35670ac42ff807ae
Haley Hamill <hamillhr@gmail.com>
2021-12-13 17:12:52 -0500

    destructured arrays

--- supermarketAppReact/supermarket/src/Practice.js
+++ supermarketAppReact/supermarket/src/Practice.js
@@ -1,6 +1,10 @@
-import React from "react";
+const dimensions = [20, 5];
+const width = dimensions[0];
+const height = dimensions[1];
 
-export default function Button(props) {
-    const {children, ...rest} = props;
-    return <button {...rest}>{children}</button>
-}
+const dimensions = [20, 5];
+const [width, height] = dimensions;
+
+
+const point = [3.3124, 14.52028, 10];
+const [lat, lng, elevation] = point;
