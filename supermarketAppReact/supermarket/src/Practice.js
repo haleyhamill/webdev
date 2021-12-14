a22fa51481fe5778edf53cc6cd1632de63d7a203
Haley Hamill <hamillhr@gmail.com>
2021-12-14 14:36:30 -0500

    added a state variable

--- supermarketAppReact/supermarket/src/Practice.js
+++ supermarketAppReact/supermarket/src/Practice.js
@@ -1,7 +1,14 @@
 import React, {useState} from "react";
 
 function Stopwatch() {
+    //hooks have to be at the top
     const [seconds, setSeconds] = useState(0);
 
-    return <div>{seconds}</div>
+    return(
+        <>
+        <h2>{seconds}</h2>
+        {/*increment seconds state by 1 when you click on the button*/}
+        <button onClick={() => setSeconds(seconds + 1)}>Click to add 1</button>
+        </>
+    );
 }
