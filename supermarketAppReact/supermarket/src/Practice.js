f9bd6b3cfcd656e4250a69f32cdc62c25124a125
Haley Hamill <hamillhr@gmail.com>
2021-12-15 17:16:12 -0500

    added ternary operator

--- supermarketAppReact/supermarket/src/Practice.js
+++ supermarketAppReact/supermarket/src/Practice.js
@@ -1,21 +1,10 @@
 import React from "react";
-import {render} from "react-dom";
-import NoPermission from "./NoPermission.js";
 
-function Admin(props) {
-    if (props.userType === "admin") {
-        return( 
-        <div>
-            <h1>Welcome Admin</h1>
-            <p>The Admin portal allows you to manage all your items</p>
-        </div>)
-    } else {
-        return <NoPermission />
+function App(props) {
+    const loginButton = <button>Login</button>;
+    const signupButton = <button>Signup</button>;
+    if (props.existingUser) {
+        return <div className="app-container">{loginButton}</div>;
     }
+    return <div className="app-container">{props.existingUser ? loginButton: signupButton}</div>;
 }
-
-// Sample usage (do not modify)
-render(<>
-    <Admin userType="admin" />
-    <Admin userType="client" />
-</>, document.querySelector("#react-root"));
