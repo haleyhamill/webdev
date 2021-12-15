98985f8cc7d98669d789f54c848bb4076ee04c69
Haley Hamill <hamillhr@gmail.com>
2021-12-14 21:04:59 -0500

    conditional state changes with props

--- supermarketAppReact/supermarket/src/Practice.js
+++ supermarketAppReact/supermarket/src/Practice.js
@@ -1,5 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 
-function App() {
-    return <input onKeyDown={() => console.log("Changed")} placeholder="Enter your name" />;
+function Counter(props) {
+    const [counter, setCounter] = useState(0);
+
+    function handleIncrementClick() {
+        if (props.enabled) {
+            setCounter(counter + 1);
+        }
+    }
+    
+    return(
+        <>
+        <h2>{counter} times clicked</h2>
+        <button onClick={handleIncrementClick}>Add 1</button>
+        </>
+    );
 }
