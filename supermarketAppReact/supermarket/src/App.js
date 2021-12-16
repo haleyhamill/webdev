6f89e9d526f90678b6d1156bea16e0b5433a985e
Haley Hamill <hamillhr@gmail.com>
2021-12-15 22:20:46 -0500

    added hook

--- supermarketAppReact/supermarket/src/App.js
+++ supermarketAppReact/supermarket/src/App.js
@@ -1,14 +1,26 @@
 import './App.css';
 import Button from './Button';
+import Input from './Input';
 
 export default function App() {
   function handleButtonClick() {
       console.log("Button clicked");
   }
 
-   return (<div style={{display: "flex", alignItems: "center", gap: "20px", flexWrap: "wrap"}}>
+  function handleLastNameInput() {
+    console.log("Last name changed");
+  }
+
+   return (<>
+   <div style={{display: "flex", alignItems: "center", gap: "20px", flexWrap: "wrap"}}>
       <Button>Normal</Button>
       <Button outline>Outline</Button>
       <Button className="extra-class" onClick={handleButtonClick}>Customizable</Button>
-  </div>);
+  </div>
+  <div style={{display: "flex", alignItems: "center", gap: "20px", flexWrap: "wrap"}}>
+      <Input placeholder="First name" />
+      <Input placeholder="Last name" onInput={handleLastNameInput} />
+      <Input placeholder="Email" type="email" required />
+  </div>
+  </>);
 }
--- supermarketAppReact/supermarket/src/Practice.js
+++ supermarketAppReact/supermarket/src/Practice.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
+import {render} from "react-dom";
 
-function App(props) {
-    const loginButton = <button>Login</button>;
-    const signupButton = <button>Signup</button>;
-    if (props.existingUser) {
-        return <div className="app-container">{loginButton}</div>;
+function Counter(props) {
+    const [count, setCount] = useState(5);
+
+    function handleCounterClick() {
+        setCount(count - props.enabled)
     }
-    return <div className="app-container">{props.existingUser ? loginButton: signupButton}</div>;
+
+    return <>
+        <h2>Attempts remaining: {count}</h2>
+        <button onClick={handleCounterClick}>Count down</button>
+    </>;
 }
+
+// Sample usage (do not modify)
+render(<>
+    <Counter enabled={true} />
+    <Counter enabled={false} />
+</>, document.querySelector("#react-root"));
