2cb8e673048fde6275ea968e85f187cf17dde314
Haley Hamill <hamillhr@gmail.com>
2021-12-17 14:34:01 -0500

    added map

--- supermarketAppReact/supermarket/src/Practice.js
+++ supermarketAppReact/supermarket/src/Practice.js
@@ -1,42 +1,32 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useLayoutEffect} from "react";
 import {render} from "react-dom";
 
-function Countdown() {
-    const [count, setCount] = useState(5);
-    const [lives, setLives] = useState(3);
+function App() {
+    const [random, setRandom] = useState(Math.random());
+    mapboxgl.accessToken = "pk.eyJ1IjoieG94b3hveG94byIsImEiOiJja3ZjcmFwZW05aHloMm5waDVxeTc4dW5iIn0.JQpWtubaYBPXqL2aJGbAzA";
 
-    useEffect(() => {
-        console.log("first render");
-
-        return () => { console.log("will be removed") }
+    useLayoutEffect(() => {
+        const map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/mapbox/dark-v10',
+            center: [12.567898, 55.675830],
+            zoom: 9
+        });
     }, []);
 
-    useEffect(() => {
-        console.log("count changed")
-    }, [count]);
-
-    useEffect(() => {
-        console.log("lives changed")
-    }, [lives]);
-
-    useEffect(() => {
-        console.log("count or lives changed")
-    }, [count, lives]);
-
-    function handleCountdownClick() {
-        if (count > 0){
-            setCount(count - 1);
-        }else if (count === 0) {
-            setCount(5);
-            setLives(lives - 1);
-        }
-    }
-
-    return <>
-        <h2>Attempts remaining: {count}</h2>
-        <button onClick={handleCountdownClick}>Count down</button>
-        <h3>Lives remaining: {lives}</h3>
+      return <>
+        <button id="re-render" onClick={() => setRandom(Math.random())}>Re-render</button>
+        <div id="map"></div>
     </>;
 }
 
-render(<Countdown />, document.querySelector("#react-root"));
+// Do NOT modify the code below
+// Special map loading setup
+// specific to react-tutorial.app
+const script = document.createElement("script");
+script.src = "https://api.mapbox.com/mapbox-gl-js/v1.8.1/mapbox-gl.js";
+script.onload = () => {
+    render(<App />, document.querySelector("#react-root"));
+}
+
+document.body.appendChild(script)
