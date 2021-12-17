ccc153d57842d322fb1cb13c9e0ddab278456380
Haley Hamill <hamillhr@gmail.com>
2021-12-17 13:32:07 -0500

    added clean up

--- supermarketAppReact/supermarket/src/Practice.js
+++ supermarketAppReact/supermarket/src/Practice.js
@@ -1,19 +1,26 @@
 import React, {useEffect, useState} from "react";
+import {render} from "react-dom";
 
 function App() {
-  const [counter, setCounter] = useState(0);
+    const [counter, setCounter] = useState(0);
 
-  useEffect(() => {
-    console.log("effect running");
-    return () => {
-      console.log("effect cleaning up");
-    }
-  })
+    useEffect(() => {
+        document.title = "Welcome";
+        const timerId = setTimeout(() => {
+            document.title = "Still there?";
+        }, 1000);
 
-  return (
+        return(() => {
+            clearTimeout(timerId)
+        });
+    });
+
+    return (
     <>
-      <h2>{counter}</h2>
-      <button onClick={() => setCounter(prevCounter => prevCounter + 1)}>Add</button>
+        <h2>{counter}</h2>
+        <button onClick={() => setCounter(prevCounter => prevCounter + 1)}>Add</button>
     </>
-  );
+    );
 }
+
+render(<App />, document.querySelector("#react-root"));
