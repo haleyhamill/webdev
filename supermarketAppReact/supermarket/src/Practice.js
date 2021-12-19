dc9426b7c6526a8649d1e647fb42fcb0fd5c8c9f
Haley Hamill <hamillhr@gmail.com>
2021-12-18 23:44:27 -0500

    added local time string

--- supermarketAppReact/supermarket/src/Practice.js
+++ supermarketAppReact/supermarket/src/Practice.js
@@ -1,10 +1,12 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import {render} from "react-dom";
 
 function Clock() {
     const [date, setDate] = useState(new Date());
 
+    useEffect(() => {
+        setInterval(() => setDate(new Date()), 1000)
+    })
+
     return <h2>{date.toLocaleTimeString()}</h2>;
 }
-
-render(<Clock />, document.querySelector("#react-root"));
