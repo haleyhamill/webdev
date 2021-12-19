ed771519b24ff342140768e7fd79f5301a085516
Haley Hamill <hamillhr@gmail.com>
2021-12-18 23:39:11 -0500

    clock boiler

--- supermarketAppReact/supermarket/src/Practice.js
+++ supermarketAppReact/supermarket/src/Practice.js
@@ -1,32 +1,8 @@
-import React, {useState, useLayoutEffect} from "react";
+import React from "react";
 import {render} from "react-dom";
 
-function App() {
-    const [random, setRandom] = useState(Math.random());
-    mapboxgl.accessToken = "pk.eyJ1IjoieG94b3hveG94byIsImEiOiJja3ZjcmFwZW05aHloMm5waDVxeTc4dW5iIn0.JQpWtubaYBPXqL2aJGbAzA";
-
-    useLayoutEffect(() => {
-        const map = new mapboxgl.Map({
-            container: 'map',
-            style: 'mapbox://styles/mapbox/dark-v10',
-            center: [12.567898, 55.675830],
-            zoom: 9
-        });
-    }, []);
-
-      return <>
-        <button id="re-render" onClick={() => setRandom(Math.random())}>Re-render</button>
-        <div id="map"></div>
-    </>;
+function Clock() {
+    return <h2>time here</h2>;
 }
 
-// Do NOT modify the code below
-// Special map loading setup
-// specific to react-tutorial.app
-const script = document.createElement("script");
-script.src = "https://api.mapbox.com/mapbox-gl-js/v1.8.1/mapbox-gl.js";
-script.onload = () => {
-    render(<App />, document.querySelector("#react-root"));
-}
-
-document.body.appendChild(script)
+render(<Clock />, document.querySelector("#react-root"));
