99311281d8e99133e17c663b9256525621c54491
Haley Hamill <hamillhr@gmail.com>
2021-12-20 10:24:01 -0500

    added if condition

--- supermarketAppReact/supermarket/src/Practice.js
+++ supermarketAppReact/supermarket/src/Practice.js
@@ -1,12 +1,28 @@
-// URL: https://react-tutorial-demo.firebaseio.com/users.json
+import React, {useState, useEffect} from "react";
+import {render} from "react-dom";
 
-function init() {
+function App() {
+  const [users, setUsers] = useState();
+
+  useEffect(() => {
     fetch("https://react-tutorial-demo.firebaseio.com/users.json")
     .then(response => response.json())
     .then(data => {
         console.log(data);
-    })
+        setUsers(data);
+    });
+  }, []);
 
+  if (!users) {
+    return null
+  }
+
+  return <>
+    <h1>Users ({users.length})</h1>
+    <ul>
+        {users.map(user => <li key={user.id}>{user.name}</li>)}
+    </ul>
+  </>
 }
 
-init();
+render(<App />, document.querySelector("#react-root"));
