dcae05ffc5d4ac1727d039dfe806475994e1963c
Haley Hamill <hamillhr@gmail.com>
2021-12-19 21:23:40 -0500

    persisted online store admin

--- supermarketAppReact/supermarket/src/Practice.js
+++ supermarketAppReact/supermarket/src/Practice.js
@@ -1,24 +1,57 @@
 import React, {useState, useEffect} from "react";
-import {render} from "react-dom";
+import ProductsList from "./ProductsList.js";
+import AddProductForm from "./AddProductForm.js";
 
-function App() {
-    const [darkTheme, setDarkTheme] = useState(true);
-
-    const className = darkTheme ? "dark" : "light";
+export default function StoreFront() {
+    const [products, setProducts] = useState(() => {
+        const prods = localStorage.getItem("products")
+        return prods ? JSON.parse(prods) : []
+    });
+    const [name, setName] = useState("");
+    const [description, setDescription] = useState("");
+    const [validation, setValidation] = useState("");
 
     useEffect(() => {
+        localStorage.setItem("products", JSON.stringify(products))
+    }, [products]);
 
-        localStorage.setItem("dark_theme", darkTheme)
-    }, [darkTheme]);
+    function handleFormSubmit(event) {
+        event.preventDefault();
 
-    function handleToggleClick() {
-        setDarkTheme(!darkTheme);
+        if (!name) {
+            setValidation("Please enter a name");
+            return ;
+        }
+        if (!description){
+            setValidation("Please enter a description");
+            return ;
+        }
+        setProducts([...products, {
+            id: products.length + 1,
+            name: name,
+            description, description
+        }]);
+        setName("");
+        setDescription("");
+        setValidation("");
+
     }
 
-    return <div className={className}>
-        <h1>Welcome</h1>
-        <button onClick={handleToggleClick}>Toggle theme</button>
-    </div>;
-}
+    function handleNameChange(event) {
+        setName(event.target.value);
+    }
 
-render(<App />, document.querySelector("#react-root"));
+    function handleDescriptionChange(event) {
+        setDescription(event.target.value);
+    }
+
+    function handleDeleteClick(id) {
+        setProducts(products.filter(product => product.id !== id));
+    }
+
+    return <>
+        <AddProductForm name={name} description={description} validation={validation} onNameChange={handleNameChange} onDescriptionChange={handleDescriptionChange} onFormSubmit={handleFormSubmit} />
+        <div>{products.length === 0 && <p>Add your first product</p>}</div>
+        <ProductsList products={products} onDeleteClick={handleDeleteClick} />
+    </>;
+}
