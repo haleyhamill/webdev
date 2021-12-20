9d83adcb7c026afe7f29af08c6633cbdf775bbec
Haley Hamill <hamillhr@gmail.com>
2021-12-19 19:10:36 -0500

    added second state

--- supermarketAppReact/supermarket/src/Practice.js
+++ supermarketAppReact/supermarket/src/Practice.js
@@ -1,16 +1,19 @@
+import React, {useState, useEffect} from "react";
+import {render} from "react-dom";
+
 function Stopwatch() {
     const [counter, setCounter] = useState(0);
     const [running, setRunning] = useState(false);
 
-    if (running) {
     useEffect(() => {
-        let timerId = setTimeout(() => {
+        if (running) {
+        const timerId = setTimeout(() => {
             setCounter(prevCounter => prevCounter + 1);
         }, 1000);
         return () => {
             clearTimeout(timerId)
-        }
-    })};
+        }}
+    });
 
     function handleButtonClick() {
         setRunning(prevValue => !prevValue)
@@ -19,5 +22,8 @@ function Stopwatch() {
     return <>
         <h2>{counter}</h2>
         <button onClick={handleButtonClick}>Start / Pause</button>
+        <button onClick={() => setCounter(0)}>Stop</button>
     </>
 }
+
+render(<Stopwatch />, document.querySelector("#react-root"));
