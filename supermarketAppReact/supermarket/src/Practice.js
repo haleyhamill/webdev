b17bd9ba988538398529b28a03c64b92a4e3d48d
Haley Hamill <hamillhr@gmail.com>
2021-12-19 19:57:57 -0500

    added mapbox

--- supermarketAppReact/supermarket/src/Practice.js
+++ supermarketAppReact/supermarket/src/Practice.js
@@ -1,29 +1,57 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useLayoutEffect} from "react";
 import {render} from "react-dom";
 
-function Stopwatch() {
-    const [counter, setCounter] = useState(0);
-    const [running, setRunning] = useState(false);
+function App() {
+    const [marker, setMarker] = useState();
+    mapboxgl.accessToken = "pk.eyJ1IjoieG94b3hveG94byIsImEiOiJja3ZjcmFwZW05aHloMm5waDVxeTc4dW5iIn0.JQpWtubaYBPXqL2aJGbAzA";
 
-    useEffect(() => {
-        if (running) {
-        const timerId = setTimeout(() => {
-            setCounter(prevCounter => prevCounter + 1);
-        }, 1000);
-        return () => {
-            clearTimeout(timerId)
-        }}
-    });
+    useLayoutEffect(() => {
+        const map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/mapbox/dark-v10',
+            center:  [ 12.567898, 55.67583 ],
+            zoom: 9
+        })
 
-    function handleButtonClick() {
-        setRunning(prevValue => !prevValue)
+        const marker = new mapboxgl.Marker()
+        .setLngLat([12.567898, 55.67583])
+        .addTo(map);
+
+        setMarker(marker);
+    }, []);
+
+    function handleListChange(event) {
+        event.preventDefault();
+        marker.setLngLat(stores[event.target.value])
     }
 
+    const stores = {
+        central: [12.567898, 55.67583],
+        norrebro: [12.553806, 55.699299],
+        airport: [12.650784, 55.618042]
+    }
+
+
     return <>
-        <h2>{counter}</h2>
-        <button onClick={handleButtonClick}>Start / Pause</button>
-        <button onClick={() => setCounter(0)}>Stop</button>
-    </>
+        <div className="map-overlay">
+            <h3>Choose store: </h3>
+            <select onChange={handleListChange}>
+                <option value="central">Central station</option>
+                <option value="norrebro">Norrebro street</option>
+                <option value="airport">CPH Airport</option>
+            </select>
+        </div>
+        <div id="map"></div>
+    </>;
 }
 
-render(<Stopwatch />, document.querySelector("#react-root"));
+// Do NOT modify the code below
+// Special map loading setup
+// specific to react-tutorial.app
+const script = document.createElement("script");
+script.src = "https://api.mapbox.com/mapbox-gl-js/v1.8.1/mapbox-gl.js";
+script.onload = () => {
+    render(<App />, document.querySelector("#react-root"));
+}
+
+document.body.appendChild(script)
