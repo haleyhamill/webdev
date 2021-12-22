3df1e3e4127543a74b527b8cdefa09db845928b8
Haley Hamill <hamillhr@gmail.com>
2021-12-21 20:43:11 -0500

    added async

--- supermarketAppReact/supermarket/src/Practice.js
+++ supermarketAppReact/supermarket/src/Practice.js
@@ -1,28 +1,9 @@
-import React, {useState, useEffect} from "react";
-import {render} from "react-dom";
+import React from "react";
 
 function App() {
-  const [users, setUsers] = useState();
+    async function handleButtonClick() {
+        
+    }
 
-  useEffect(() => {
-    (async () => {
-      const response = await fetch(`https://react-tutorial-demo.firebaseio.com/users.json`)
-      const data = await response.json()
-      console.log(data)
-      setUsers(data)
-    })();
-  }, []);
-
-  if (!users) {
-    return null;
-  }
-
-  return <>
-    <h1>Users ({users.length})</h1>
-    <ul>
-        {users.map(user => <li key={user.id}>{user.name}</li>)}
-    </ul>
-  </>
+    return <button onClick={handleButtonClick}>Load data</button>;
 }
-
-render(<App />, document.querySelector("#react-root"));
