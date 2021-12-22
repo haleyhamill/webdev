07c21bc2805857026c2f675aeae16023397cf52f
Haley Hamill <hamillhr@gmail.com>
2021-12-21 20:59:56 -0500

    added async await

--- supermarketAppReact/supermarket/src/Practice.js
+++ supermarketAppReact/supermarket/src/Practice.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
+import {render} from "react-dom";
 
 function App() {
-    async function handleButtonClick() {
-        
+    const [users, setUsers] = useState();
+    const [isLoading, setIsLoading] = useState(false);
+
+    function handleButtonClick() {
+        setIsLoading(true)
+        fetch(`https://react-tutorial-demo.firebaseio.com/users.json`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            setUsers(data)
+        })
+        .catch(error => console.log(error))
+        .finally(() => setIsLoading(false))
     }
 
-    return <button onClick={handleButtonClick}>Load data</button>;
+    return (<>
+        <button onClick={handleButtonClick} disabled={isLoading}>Load users</button>
+        <h1>Users</h1>
+        <ul>
+            {users && users.map(user => <li key={user.id}>{user.name}</li>)}
+        </ul>
+    </>);
 }
+
+render(<App />, document.querySelector("#react-root"));
