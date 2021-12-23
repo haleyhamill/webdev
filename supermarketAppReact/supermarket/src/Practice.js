5868feb3499290a87eab0fc1bd549b1ca737106f
Haley Hamill <hamillhr@gmail.com>
2021-12-22 20:24:10 -0500

    added post

--- supermarketAppReact/supermarket/src/Practice.js
+++ supermarketAppReact/supermarket/src/Practice.js
@@ -1,11 +1,28 @@
-fetch("https://react-tutorial-demo.firebaseio.com/grades.json", {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json"
-    },
-    body: JSON.stringify({grade: 50})
-})
-.then(response => response.json())
-.then(data => {
-    console.log(data);
-});
+import React, {useState} from "react";
+
+function App() {
+    const [number, setNumber] = useState(0);
+
+    function handleFormSubmit(event) {
+        event.preventDefault();
+
+        fetch("https://react-tutorial-demo.firebaseio.com/grades.json", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({grade: number})
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log("Grade added");
+            console.log(data);
+        });
+    }
+
+    return <form onSubmit={handleFormSubmit}>
+        <input type="number" value={number} name="grade" onChange={event => setNumber(event.target.value)} placeholder="Enter the grade" />
+        <input type="submit" />
+    </form>
+    ;
+}
