0c93f3c27a61fd97b152829c0db61cceaec00f3b
Haley Hamill <hamillhr@gmail.com>
2021-12-24 14:59:57 -0500

    added ref

--- supermarketAppReact/supermarket/src/Practice.js
+++ supermarketAppReact/supermarket/src/Practice.js
@@ -1,23 +1,17 @@
-import React, {useState} from "react";
+import React, {useRef, useEffect} from "react";
 import {render} from "react-dom";
-import useFetch from "./useFetch.js";
 
-function GradeForm() {
-    const [grade, setGrade] = useState(0);
-    const {post} = useFetch("https://api.learnjavascript.online/demo/react/")
+function Form() {
+    const inputRef = useRef();
 
-    function handleFormSubmit(event) {
-        event.preventDefault();
-        post("grades", {"grade": grade});
-        setGrade(0);
-    }
+    useEffect(() => {
+        inputRef.current.focus()
+    }, [])
 
-    return (
-        <form onSubmit={handleFormSubmit}>
-            <input type="number" value={grade} name="grade" onChange={event => setGrade(event.target.value)} placeholder="Enter the grade" />
-            <input type="submit" />
-        </form>
-    );
+    return (<form>
+        <h2>Welcome</h2>
+        <input ref={inputRef} type="text" placeholder="Enter your name" />
+    </form>);
 }
 
-render(<GradeForm />, document.querySelector("#react-root"));
+render(<Form/>, document.querySelector("#react-root"));
