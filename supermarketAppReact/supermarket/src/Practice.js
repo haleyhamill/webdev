025a122c47b6bb80a84c8ad8b590995e93909f2f
Haley Hamill <hamillhr@gmail.com>
2021-12-24 11:29:35 -0500

    added useFetch

--- supermarketAppReact/supermarket/src/Practice.js
+++ supermarketAppReact/supermarket/src/Practice.js
@@ -1,15 +1,9 @@
-import React, {useState} from "react";
-import {render} from "react-dom";
-import useProductCounter from "./useProductCounter.js";
+export default function useFetch(baseUrl) {
+    function get(url) {
+        const data = fetch(baseUrl + url)
+            .then(response => response.json())
+        return data
+    }
 
-function App() {
-    const {count, increment, decrement} = useProductCounter();
-
-    return <>
-        <h2>{count}</h2>
-        <button onClick={increment}>+</button>
-        <button onClick={decrement}>-</button>
-    </>;
-}
-
-render(<App />, document.querySelector("#react-root"));
+    return { get }
+};
