3dc2181fdfffed84696069383a125ac6ad0c7516
Haley Hamill <hamillhr@gmail.com>
2021-12-24 13:51:35 -0500

    added loader

--- supermarketAppReact/supermarket/src/Practice.js
+++ supermarketAppReact/supermarket/src/Practice.js
@@ -1,20 +1,21 @@
-import React, {useState, useEffect} from "react";
-import {render} from "react-dom";
+import React, {useEffect, useState} from "react";
 import useFetch from "./useFetch.js";
+import {render} from "react-dom";
+import Loader from "./Loader.js";
 
 function App() {
-    const [users, setUsers] = useState();
-    const {get, loading} = useFetch("https://react-tutorial-demo.firebaseio.com/")
+  const [users, setUsers] = useState();
+  const {get, loading} = useFetch("https://react-tutorial-demo.firebaseio.com/");
+
     useEffect(() => {
         get("users.json").then(data => {
-            console.log(data)
-            setUsers(data)
-        })
-        .catch(error => console.log(error))
+            setUsers(data);
+        }).catch(error => console.error(error));
     }, []);
 
-  return <>
+    return <>
         <h1>Users</h1>
+        {loading && <Loader />}
         <ul>
             {users && users.map(user => <li key={user.id}>{user.name}</li>)}
         </ul>
