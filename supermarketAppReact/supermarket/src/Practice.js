b518906b92613daeb3a3530d542345100cbafb64
Haley Hamill <hamillhr@gmail.com>
2021-12-25 15:12:29 -0500

    persisted the event

--- supermarketAppReact/supermarket/src/Practice.js
+++ supermarketAppReact/supermarket/src/Practice.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 import {render} from "react-dom";
-import {EN} from "./translations.js";
 
-function App() {
-    return (<>
-        <div dangerouslySetInnerHTML={{__html: EN.welcome}}></div>
-        <button dangerouslySetInnerHTML={{__html: EN.login}}></button>
-    </>);
+function CurrencySelector() {
+    const [currency, setCurrency] = useState("");
+    const [rate, setRate] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+
+    function handleCurrencyChange(event) {
+        event.persist()
+        setIsLoading(true);
+        fetch(`https://react-tutorial-demo.firebaseio.com/currencies/${event.target.value}.json`)
+            .then(response => response.json())
+            .then(data => {
+                setCurrency(event.target.value);
+            })
+            .catch(error => console.log(error))
+            .finally(() => {
+                setIsLoading(false);
+            });
+    }
+
+    return <>
+        <h2>Currency selector</h2>
+        <select onChange={handleCurrencyChange} disabled={isLoading}>
+            <option value="">Select a currency</option>
+            <option value="usd">USD</option>
+            <option value="eur">EUR</option>
+            <option value="cad">CAD</option>
+        </select>
+        <h3>Chosen currency: <span id="chosen">{currency.toUpperCase()}</span></h3>
+    </>;
 }
 
-render(<App />, document.querySelector("#react-root"));
+render(<CurrencySelector />, document.querySelector("#react-root"));
