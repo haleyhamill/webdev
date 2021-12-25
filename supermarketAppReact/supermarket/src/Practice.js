f652d5e5e61eda85dd0a16e17130b1dd1c939f66
Haley Hamill <hamillhr@gmail.com>
2021-12-24 21:09:52 -0500

    dangerously set html

--- supermarketAppReact/supermarket/src/Practice.js
+++ supermarketAppReact/supermarket/src/Practice.js
@@ -1,23 +1,12 @@
-import React, {useRef, useEffect} from "react";
+import React from "react";
 import {render} from "react-dom";
+import {EN} from "./translations.js";
 
-function VideoPlayer() {
-    const videoRef = useRef()
-
-    function handlePlayClick() {
-        videoRef.current.play()
-    }
-
-    function handlePauseClick() {
-        videoRef.current.pause()
-    }
-
+function App() {
     return (<>
-        <h2>Video player</h2>
-        <button onClick={handlePlayClick}>Play</button>
-        <button onClick={handlePauseClick}>Pause</button>
-        <video ref={videoRef} width="300" controls src="https://res.cloudinary.com/dbfn5lnvx/video/upload/v1599125933/react-tutorial/ref-video-player-challenge.mp4" />
+        <div dangerouslySetInnerHTML={{__html: EN.welcome}}></div>
+        <button dangerouslySetInnerHTML={{__html: EN.login}}></button>
     </>);
 }
 
-render(<VideoPlayer/>, document.querySelector("#react-root"));
+render(<App />, document.querySelector("#react-root"));
