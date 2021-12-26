13a44d1594fee0181a3fdf8034123c1220b1e21e
Haley Hamill <hamillhr@gmail.com>
2021-12-25 21:29:19 -0500

    added navlinks

--- supermarketAppReact/supermarket/src/Practice.js
+++ supermarketAppReact/supermarket/src/Practice.js
@@ -1,24 +1,49 @@
 import React from "react";
+import {BrowserRouter, Switch, Route, NavLink} from "react-router-dom";
 import {render} from "react-dom";
-import StoreFront from "./StoreFront.js";
-import {BrowserRouter, Route, Switch, Link} from "react-router-dom";
-import ProductDetails from "./ProductDetails.js";
+import Home from "./Home.js";
+import Products from "./Products.js";
+import Team from "./Team.js";
+import NotFound from "./NotFound.js";
 
 function App() {
-    return (
-        <BrowserRouter>
-            <main>
-                <Switch>
-                    <Route exact path="/">
-                        <StoreFront/>
-                    </Route>
-                    <Route exact path="/products/:id">
-                        <ProductDetails/>
-                    </Route>
-                </Switch>
-            </main>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+        <nav>
+          <ul>
+            <li>
+              <NavLink activeClassName="nav-active" to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink activeClassName="nav-active" to="/products">Products</NavLink>
+            </li>
+            <li>
+              <NavLink activeClassName="nav-active" to="/team">Team</NavLink>
+            </li>
+            <li>
+              <NavLink activeClassName="nav-active" to="/non-existing-page">Non existing page</NavLink>
+            </li>
+          </ul>
+        </nav>
+
+        <main>
+            <Switch>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+                <Route exact path="/products">
+                    <Products />
+                </Route>
+                <Route exact path="/team">
+                    <Team />
+                </Route>
+                <Route>
+                  <NotFound />
+                </Route>
+            </Switch>
+        </main>
+    </BrowserRouter>
+  );
 }
 
 render(<App />, document.querySelector("#react-root"));
