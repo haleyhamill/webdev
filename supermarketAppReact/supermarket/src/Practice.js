e614d20b07ef4fdeccc4f7150cf49248f6db3449
Haley Hamill <hamillhr@gmail.com>
2021-12-26 10:31:28 -0500

    programmatically redirected

--- supermarketAppReact/supermarket/src/Practice.js
+++ supermarketAppReact/supermarket/src/Practice.js
@@ -1,49 +1,17 @@
 import React from "react";
-import {BrowserRouter, Switch, Route, NavLink} from "react-router-dom";
-import {render} from "react-dom";
-import Home from "./Home.js";
-import Products from "./Products.js";
-import Team from "./Team.js";
-import NotFound from "./NotFound.js";
+import {useHistory} from "react-router-dom";
 
-function App() {
-  return (
-    <BrowserRouter>
-        <nav>
-          <ul>
-            <li>
-              <NavLink activeClassName="nav-active" to="/">Home</NavLink>
-            </li>
-            <li>
-              <NavLink activeClassName="nav-active" to="/products">Products</NavLink>
-            </li>
-            <li>
-              <NavLink activeClassName="nav-active" to="/team">Team</NavLink>
-            </li>
-            <li>
-              <NavLink activeClassName="nav-active" to="/non-existing-page">Non existing page</NavLink>
-            </li>
-          </ul>
-        </nav>
+export default function Home(props) {
+    const {loggedIn} = props;
+    const history = useHistory();
 
-        <main>
-            <Switch>
-                <Route exact path="/">
-                    <Home />
-                </Route>
-                <Route exact path="/products">
-                    <Products />
-                </Route>
-                <Route exact path="/team">
-                    <Team />
-                </Route>
-                <Route>
-                  <NotFound />
-                </Route>
-            </Switch>
-        </main>
-    </BrowserRouter>
-  );
+    function handleStartClick() {
+        loggedIn ? history.push("/dashboard") : history.push("/login")
+    }
+
+    return <>
+        <h2>Home: {loggedIn ? "Logged in" : "Logged out"}</h2>
+        <button onClick={handleStartClick}>Start</button>
+        <hr />
+    </>;
 }
-
-render(<App />, document.querySelector("#react-root"));
