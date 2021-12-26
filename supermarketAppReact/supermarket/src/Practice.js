be3628e1dc9e95d7912dcfad8151f41fdfd97231
Haley Hamill <hamillhr@gmail.com>
2021-12-26 12:51:39 -0500

    added useLocation

--- supermarketAppReact/supermarket/src/Practice.js
+++ supermarketAppReact/supermarket/src/Practice.js
@@ -1,17 +1,44 @@
-import React from "react";
-import {useHistory} from "react-router-dom";
+import React, {useEffect} from "react";
+import {Switch, Route, Link, useLocation} from "react-router-dom";
+import Landing from "./Landing.js";
+import Products from "./Products.js";
+import Team from "./Team.js";
 
-export default function Home(props) {
-    const {loggedIn} = props;
-    const history = useHistory();
+export default function Home() {
+  const location = useLocation();
 
-    function handleStartClick() {
-        loggedIn ? history.push("/dashboard") : history.push("/login")
-    }
+  useEffect(() => {
+    console.log(location.pathname);
+  }, [location]);
 
-    return <>
-        <h2>Home: {loggedIn ? "Logged in" : "Logged out"}</h2>
-        <button onClick={handleStartClick}>Start</button>
-        <hr />
-    </>;
+    return (<>
+        <nav>
+          <h2>You are currently viewing: {location.pathname}</h2>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/products">Products</Link>
+            </li>
+            <li>
+              <Link to="/team">Team</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <main>
+            <Switch>
+                <Route exact path="/">
+                    <Landing />
+                </Route>
+                <Route exact path="/products">
+                    <Products />
+                </Route>
+                <Route exact path="/team">
+                    <Team />
+                </Route>
+            </Switch>
+        </main>
+    </>);
 }
