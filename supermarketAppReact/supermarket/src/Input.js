77fd5e72842992323cf0ab65021797b7ca2d110a
Haley Hamill <hamillhr@gmail.com>
2021-12-27 17:56:31 -0500

    updated input

--- supermarketAppReact/supermarket/src/Input.js
+++ supermarketAppReact/supermarket/src/Input.js
@@ -2,21 +2,23 @@ import React from "react";
 import clsx from "clsx";
 
 export default function Input(props) {
-    const {type, className, ...rest} = props;
-    const types = clsx({
-        "type": !type,
-    }, type);
-    const classNames = clsx({
-        "input": true,
-    }, className);
-    
-    return(<>
+  const { className, placeholder, required, type = "text", ...rest } = props;
+
+  const classNames = clsx({ input: true }, className);
+
+  return (
     <label className="label">
-    {props.placeholder}
-        {props.required && <span className="input-required">*</span>}
-    <div>
-        <input required={props.required} className={classNames} type={types} {...rest}/>
-    </div>
+      {placeholder}
+      {required && <span className="input-required">*</span>}
+      <div>
+        <input
+          type={type}
+          placeholder={placeholder}
+          className={classNames}
+          required={required}
+          {...rest}
+        />
+      </div>
     </label>
-    </>);
+  );
 }
