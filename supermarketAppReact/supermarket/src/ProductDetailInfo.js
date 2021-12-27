815434a4d4c7d7bcc8d0d5f0265d139ef3de9faa
Haley Hamill <hamillhr@gmail.com>
2021-12-27 17:57:58 -0500

    updated product detail info

--- supermarketAppReact/supermarket/src/ProductDetailInfo.js
+++ supermarketAppReact/supermarket/src/ProductDetailInfo.js
@@ -1,8 +1,15 @@
 import React from "react";
+import Button from "./Button.js";
 
 export default function ProductDetailInfo(props) {
-  return <><p>
-  {props.description} sold at <strong>${props.price}</strong> per piece.
-</p>
-<button>${props.price}</button></>;
+  const {product, onProductAdd} = props;
+  return (
+    <>
+      <p>
+        {product.description} sold at <strong>${product.price}</strong> per
+        piece.
+      </p>
+      <Button onClick={() => onProductAdd(product)}>${product.price}</Button>
+    </>
+  );
 }
