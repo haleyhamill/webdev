c63d50116fce954974550f8661c57c8963fe7c42
Haley Hamill <hamillhr@gmail.com>
2021-12-27 17:55:28 -0500

    updated button

--- supermarketAppReact/supermarket/src/Button.js
+++ supermarketAppReact/supermarket/src/Button.js
@@ -2,11 +2,19 @@ import React from "react";
 import clsx from "clsx";
 
 export default function Button(props) {
-    const {className, ...rest} = props;
-    const classes = clsx({
-        "btn": true,
-        "btn-outline": props.outline,
-        "btn-default": !props.outline
-    }, className)
-    return <button className={classes} {...rest}>{props.children}</button>
+  const { children, outline, className, ...rest } = props;
+
+  const classNames = clsx({
+      btn: true,
+      "btn-default": !outline,
+      "btn-outline": outline,
+    },
+    className
+  );
+
+  return (
+    <button className={classNames} {...rest}>
+      {children}
+    </button>
+  );
 }
